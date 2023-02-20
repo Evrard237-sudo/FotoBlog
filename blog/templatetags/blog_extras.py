d5ac4dfbf4b424e52ae45e257b7e15228301ad8d
Evrard237-sudo <114961933+Evrard237-sudo@users.noreply.github.com>
2023-02-20 01:52:25 +0100

    Avancement

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,4 +1,9 @@
+import django.utils.timezone
 from django import template
+
+MINUTE = 60
+HOUR = 60 * MINUTE
+DAY = 24 * HOUR
 
 register = template.Library()
 
@@ -6,3 +11,19 @@ register = template.Library()
 @register.filter
 def model_type(value):
     return type(value).__name__
+
+
+@register.simple_tag(takes_context=True)
+def get_poster_display(context, user):
+    if user == context['user']:
+        return 'vous'
+    return user.username
+
+
+def get_posted_at_display(posted_at):
+    seconds_ago = (timezone.now() - posted_at).total_seconds()
+    if seconds_ago <= HOUR:
+        return f'Publié il y a {int(seconds_ago // MINUTE)} minutes.'
+    elif seconds_ago <= DAY:
+        return f'Publié il y a {int(seconds_ago // HOUR)} heures.'
+    return f'Publié le {posted_at.strftime("%d %b %y à %Hh%M")}'
